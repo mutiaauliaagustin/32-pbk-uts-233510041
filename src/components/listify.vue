73174a9dfcd7d74dfc46e7e0f2b9b4e688f0c355
Muti <@mutiaauliaagustin@student.uir.ac.id>
2025-05-05 19:58:41 +0700

    Menambahkan struktur dasar tanpa fungsi

---
+++ src/components/listify.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const showInput = ref(false);
+const activities = ref([]);
+</script>
+
+<template>
+  <div class="app-container">
+    <div class="main-content">
+      <div id="app" class="to-do">
+        <div class="header-box">  
+          <h1>Listify - Perencana Jadwal</h1>
+          <p>Kelola kegiatan harianmu dengan efisien.</p>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
